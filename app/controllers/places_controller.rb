2b7b497cd5a4425f85ccb06f8f018544c2177bf4
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-06-19 16:51:31 +0000

    connected user info to places info

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,6 @@
 class PlacesController < ApplicationController
+	before_action :authenticate_user!, :only => [:new, :create]
+
 	def index
 		@places = Place.all
 		@places = Place.paginate(:page => params[:page], :per_page => 3)
@@ -9,9 +11,11 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		Place.create(place_params)
+		current_user.places.create(place_params)
 		redirect_to root_path
 	end
+
+	private
 
 	def place_params
 		params.require(:place).permit(:name, :description, :address)

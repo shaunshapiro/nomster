1be8c2e46d8768e65d87c0f769757effc0638c21
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-06-22 05:41:48 +0000

    added ability to delete posts

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,7 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, :only => [:new, :create]
 
 	def index
-		@places = Place.all.find(:all, :order => 'name DESC')
-		@places = Place.paginate(:page => params[:page], :per_page => 5)
+		@places = Place.all.paginate(:page => params[:page], :per_page => 5).order('created_at ASC')
 	end
 
 	def new
@@ -26,6 +25,12 @@ class PlacesController < ApplicationController
 	def update
 		@place = Place.find(params[:id])
 		@place.update_attributes(place_params)
+		redirect_to root_path
+	end
+
+	def destroy
+		@place = Place.find(params[:id])
+		@place.destroy
 		redirect_to root_path
 	end
 

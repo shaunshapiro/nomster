2ed1d29e8f94660605bd1ba1a44e2d1c64e5cf54
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-06-26 20:00:57 +0000

    changed upload storage from file to fog for heroku

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -10,8 +10,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  #storage :file
+  storage :fog
 
   #def store_dir
   # {}"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"

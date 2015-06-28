045209dd5921a0c6052af83738b3562eccf7283f
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-06-28 21:27:26 +0000

    changed email notifications to send to user who created place when new comments are added to that place

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomster-shaun-shapiro.herokuapp.com"
 
-  def comment_added
-  	mail(to: "shaun.shapiro91@gmail.com",
-  		subject: "A comment has been added to your place")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+
+  	mail(to: @place_owner.email,
+  		subject: "A comment has been added to #{@place.name}")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,7 @@ Nomster::Application.configure do
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_PASSWORD']
   }
+  
   config.action_mailer.default_url_options = { :host => 'localhost:3030' }
   
   # Settings specified here will take precedence over those in config/application.rb.
